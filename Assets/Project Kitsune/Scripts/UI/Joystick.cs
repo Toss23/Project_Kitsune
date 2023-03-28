274ea991e494f2b697d185b305cd3a8de326bd2a
Alexander <95612872+Toss23@users.noreply.github.com>
2023-03-28 12:39:39 +0300

    Update Joystick.cs

--- Assets/Project Kitsune/Scripts/UI/Joystick.cs
+++ Assets/Project Kitsune/Scripts/UI/Joystick.cs
@@ -10,7 +10,7 @@ public class Joystick : MonoBehaviour
     [SerializeField] private int _maxRadiusStick = 200;
 
     private Vector3 _screenSize;
-    private Vector2 _joystickPosition;
+    private Vector3 _joystickPosition;
     private float _angle;
 
     private void Awake()
@@ -23,17 +23,71 @@ public class Joystick : MonoBehaviour
 
     private void Update()
     {
-        Vector2 stickPosition = Vector2.zero;
+        Vector3 stickPosition = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Application.isMobilePlatform)
         {
-            Vector2 mousePosition = Input.mousePosition - 0.5f * _screenSize;
-            stickPosition = mousePosition - _joystickPosition;
-            _angle = Mathf.Atan2(stickPosition.y, stickPosition.x) * Mathf.Rad2Deg;
+            // Touch control
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                    case TouchPhase.Moved:
+                    case TouchPhase.Stationary:
+                        GetStickPositionAndAngle(touch.position, out stickPosition, out _angle);
+                        OnActive?.Invoke(_angle, Time.deltaTime);
+                        break;
+                }
+            }
+        }
+        else
+        {
+            // Mouse control
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                GetStickPositionAndAngle(Input.mousePosition, out stickPosition, out _angle);
+                OnActive?.Invoke(_angle, Time.deltaTime);
+            }
 
-            OnActive?.Invoke(_angle, Time.deltaTime);
+            // Keyboard control
+            if (Input.GetKey(KeyCode.W))
+            {
+                stickPosition = new Vector3(0, _maxRadiusStick);
+                _angle = 90;
+                OnActive?.Invoke(_angle, Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                stickPosition = new Vector3(0, -_maxRadiusStick);
+                _angle = 270;
+                OnActive?.Invoke(_angle, Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                stickPosition = new Vector3(-_maxRadiusStick, 0);
+                _angle = 180;
+                OnActive?.Invoke(_angle, Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                stickPosition = new Vector3(_maxRadiusStick, 0);
+                _angle = 0;
+                OnActive?.Invoke(_angle, Time.deltaTime);
+            }
         }
 
         _stick.localPosition = Vector2.ClampMagnitude(stickPosition, _maxRadiusStick);
+    }
+
+    private void GetStickPositionAndAngle(Vector3 touchPosition, out Vector3 stickPosition, out float angle)
+    {
+        touchPosition -= 0.5f * _screenSize;
+        stickPosition = touchPosition - _joystickPosition;
+        angle = Mathf.Atan2(stickPosition.y, stickPosition.x) * Mathf.Rad2Deg;
     }
 }
