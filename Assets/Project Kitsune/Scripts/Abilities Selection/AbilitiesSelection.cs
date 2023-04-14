a8dc566bac333a1d8a9572dbe03295e2af21af55
Alexander <95612872+Toss23@users.noreply.github.com>
2023-04-15 00:29:04 +0300

    Ult Learn

--- Assets/Project Kitsune/Scripts/Abilities Selection/AbilitiesSelection.cs
+++ Assets/Project Kitsune/Scripts/Abilities Selection/AbilitiesSelection.cs
@@ -19,7 +19,7 @@ public class AbilitiesSelection
         AbilitiesState abilities = _unit.Abilities;
 
         List<int> canLevelUp = new List<int>();
-        int[] abilityIds = { -1, -1 };
+        int[] abilitiesIdsForCards = { -1, -1 };
 
         for (int i = 0; i < abilities.List.Length; i++)
         {
@@ -27,25 +27,47 @@ public class AbilitiesSelection
                 canLevelUp.Add(i);
         }
 
-        if (canLevelUp.Contains(4))
-            canLevelUp.Remove(4);
+        bool canLearnUlt = false;
+        bool allAbilityLearned = true;
 
-        if (canLevelUp.Count > 0)
+        if (canLevelUp.Contains(4))
         {
-            abilityIds[0] = UnityEngine.Random.Range(0, canLevelUp.Count - 1);
-            canLevelUp.Remove(abilityIds[0]);
+            canLevelUp.Remove(4);
+            canLearnUlt = true;
         }
 
         if (canLevelUp.Count > 0)
         {
-            abilityIds[1] = UnityEngine.Random.Range(0, canLevelUp.Count - 1);
+            allAbilityLearned = false;
+        }
+
+        if (canLevelUp.Count > 0)
+        {
+            abilitiesIdsForCards[0] = UnityEngine.Random.Range(0, canLevelUp.Count - 1);
+            canLevelUp.Remove(abilitiesIdsForCards[0]);
+        }
+
+        if (canLevelUp.Count > 0)
+        {
+            abilitiesIdsForCards[1] = UnityEngine.Random.Range(0, canLevelUp.Count - 1);
+        }
+
+        if (allAbilityLearned)
+        {
+            if (canLearnUlt)
+                abilitiesIdsForCards[0] = 4;
+        }
+        else
+        {
+            if (canLearnUlt && UnityEngine.Random.Range(0, 99) <= 9)
+                abilitiesIdsForCards[1] = 4;
         }
 
         IAbility[] abilitiesForCards = new IAbility[2];
-        abilitiesForCards[0] = abilityIds[0] != -1 ? abilities.List[abilityIds[0]] : null;
-        abilitiesForCards[1] = abilityIds[1] != -1 ? abilities.List[abilityIds[1]] : null;
+        abilitiesForCards[0] = abilitiesIdsForCards[0] != -1 ? abilities.List[abilitiesIdsForCards[0]] : null;
+        abilitiesForCards[1] = abilitiesIdsForCards[1] != -1 ? abilities.List[abilitiesIdsForCards[1]] : null;
 
-        if (abilityIds[0] == -1 & abilityIds[1] == -1)
+        if (abilitiesIdsForCards[0] == -1 & abilitiesIdsForCards[1] == -1)
             OnSelectedAbility?.Invoke(null);
         else
             OnAbilitiesListGenerated?.Invoke(abilitiesForCards);
